89ded8a78694b774350fea85c420dc001bbf177b
AxelWard <ajweagle@hotmail.com>
2019-09-05 01:32:10 -0600

    Preprocessing for all file types
    Added preprocessing for all files (cancer-voting)

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -3,8 +3,11 @@ public class Main {
 	public static void main(String[] args)
 	{
 		DataPreprocessor dp = new DataPreprocessor();
-		dp.createProcessedCSV("glass.data", "glass.csv");
-		
+		dp.createProcessedCSV("DataSets/Glass/glass.data", "glass.csv", "glass");
+		dp.createProcessedCSV("DataSets/BreastCancer/breast-cancer-wisconsin.data", "cancer.csv", "cancer");
+		dp.createProcessedCSV("DataSets/Iris/iris.data", "iris.csv", "iris");
+		dp.createProcessedCSV("DataSets/Soybean/soybean-small.data", "soybeans.csv", "soybean");
+		dp.createProcessedCSV("DataSets/Vote/house-votes-84.data", "votes.csv", "vote");
 		System.out.println("Ran!");
 	}
 }
