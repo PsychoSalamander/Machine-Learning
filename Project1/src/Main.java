665c61e5086af54b31514936fd9178cd4d7ba248
Beauteng <beauteng@gmail.com>
2019-09-10 11:53:44 -0600

    Updating Main Manually...

---
+++ Project1/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+	public static void main(String[] args)
+	{
+		
+		DataPreprocessor dp = new DataPreprocessor();
+		dp.createProcessedCSV("DataSets/Glass/glass.data", "glass.csv", "glass");
+		dp.createProcessedCSV("DataSets/BreastCancer/breast-cancer-wisconsin.data", "cancer.csv", "cancer");
+		dp.createProcessedCSV("DataSets/Iris/iris.data", "iris.csv", "iris");
+		dp.createProcessedCSV("DataSets/Soybean/soybean-small.data", "soybeans.csv", "soybean");
+		dp.createProcessedCSV("DataSets/Vote/house-votes-84.data", "votes.csv", "vote");
+		
+		DataReader dr = new DataReader();
+		int cancerData[][] = dr.readArrayFromCSV("glass.csv");
+		
+		if(cancerData != null)
+		{
+			for(int i = 0; i < cancerData.length; i++)
+			{
+				for(int j = 0; j < cancerData[0].length; j++)
+				{
+					System.out.print("[" + cancerData[i][j] + "]");
+				}
+			System.out.print("\n");
+			}
+		}
+		
+		System.out.println("Ran!");
+	}
+}
