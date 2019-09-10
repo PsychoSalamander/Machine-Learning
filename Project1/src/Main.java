706aacc7c3d46cfafaf20ebf7846c7c81080bb15
Beauteng <beauteng@gmail.com>
2019-09-10 11:45:49 -0600

    fixed integration error

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -1,34 +1,7 @@
-package project1;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
 
-	public static void Main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		reader();
-		
-	}
-
-	public static void reader() throws IOException
-	{
-		Path path = Paths.get("src/glass.data");
-
-		BufferedReader reader = Files.newBufferedReader(path);
-		String line = reader.readLine();
-		StringBuilder builder = new StringBuilder();
-		//frankie sucks
-		while(line != null)
-		{
-			System.out.println(line);
-			builder.append(line);
-			line = reader.readLine();
-		}
-		
+	public static void Main(String[] args) {
 		
 	}
 }
