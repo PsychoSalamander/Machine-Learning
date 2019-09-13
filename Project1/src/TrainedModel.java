42ccb06c3d6e5e3a802f93d34256ccbe9c3307ef
AxelWard <ajweagle@hotmail.com>
2019-09-13 09:25:47 -0600

    TrainedModel Structure

--- Project1/src/TrainedModel.java
+++ Project1/src/TrainedModel.java
@@ -1,15 +1,14 @@
+import java.util.ArrayList;
 
 public class TrainedModel {
 	
 	// Class to hold all of the training data to be used by the test classed.
 	
-	public double[][] priors;
-	public double[][] evidence;
-	public double[][] evidenceL;
+	double percentages[][][];
+	ArrayList<ArrayList<Integer>> attributes;
+	ArrayList<Integer> classes;
 	
 	TrainedModel() {
-		priors = new double[1][1];
-		evidence = new double[1][1];
-		evidenceL = new double[1][1];
+		
 	}
 }
