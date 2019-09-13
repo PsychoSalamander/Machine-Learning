a71efc494d0f3fae8954d475c3d0fdece737bdc7
PsychoSalamander <36999417+PsychoSalamander@users.noreply.github.com>
2019-09-12 23:05:16 -0600

    More updates
    calculates probability of attribute per class

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -144,7 +144,7 @@ public class Main {
 		double[][] evidence = new double[2][attList.size()];
 		for(int k = 0; k < att[0].length; k++) {
 			evidence[0][k] = (double)att[0][k];
-			evidence[1][k] = (double)att[1][k]/NOR; // probability (likelihood) of the evidence (size of 24) 
+			evidence[1][k] = (double)att[1][k]/NOR; // probability (likelihood) of the evidence
 			System.out.println(evidence[1][k]);
 			//vertical columns
 			
@@ -174,6 +174,7 @@ public class Main {
 					for(int d = 0; d < evidenceList.size(); d++) {
 						if(evidenceList.get(d) == arr[x][y]) {
 								if(identifierList.get(d) == arr[x][NOC]) {
+									
 									int temp = evidenceFreqList.get(d);
 									evidenceFreqList.set(d, ++temp);
 									
@@ -200,9 +201,12 @@ public class Main {
 			
 		}
 		int tempsize = identifierList.size();
-
-		double[][] evidenceL = new double[priors[0].length][identifierList.size()];
-		for(int b = 0 ; b < tempsize; b++) {					
+		System.out.println(priors[0].length + "priors length");
+		double[][] evidenceL = new double[6][identifierList.size()];
+		for(int b = 0 ; b < tempsize; b++) {
+			for(int i = 0; i < priors[0].length; i++ ) {
+				
+			}
 			evidenceL[0][b] = identifierList.get(b);			//class type1 = 16, type2 = 20, type3 = 14, type5 = 17, type6 = 14, type7 = 20 
 			evidenceL[1][b] = evidenceList.get(b);				//attributes 
 			evidenceL[2][b] = evidenceFreqList.get(b);			//frequency the attributes occur
@@ -233,22 +237,36 @@ public class Main {
 			
 		
 			}
+		
+		int spots = 0;
+		int oneOne = 0;
 		//calculate probability per attribute given a class. every multiple of priors[0].length is that given class probability
 		double [][] probabilityPerClass = new double [1][priors[0].length*identifierList.size()];		//an array for the probability per attribute given a class
-		for(int b = 0; b < priors[0].length*identifierList.size(); b ++) {								//for loop to get every attribute calculated
+		for(int b = 0; b <= priors[0].length*identifierList.size(); b ++) {								//for loop to get every attribute calculated
 			
-			for(int oneOne = 0; oneOne < 101; oneOne++) {
-				for(int spots = 0; spots <= priors[0].length +1; spots++) {
-					if(evidenceL[3][spots] != 0) {
-						probabilityPerClass[0][b] = evidenceL[2][oneOne]  / evidenceL[3][spots];			//needed calculation (frequency of attribute / frequency of class)
-						System.out.println(evidenceL[2][oneOne] + "/" + evidenceL[3][spots]);
-						System.out.println(probabilityPerClass[0][b] + " sdf");
-						System.out.println(spots + " spots");
-						System.out.println(oneOne + " oneOne");
+			//for(int oneOne = 0; oneOne < identifierList.size(); oneOne++) {
+				//for(int spots = 0; spots <= priors[0].length +1; spots++) {
+					if((evidenceL[3][spots] != 0) || (spots <= priors[0].length) ) {
+						if(oneOne == 101) {
+							spots++;
+							oneOne = 0;
+						}
+						if((evidenceL[0][oneOne ] == spots)) {
+							probabilityPerClass[0][b] = evidenceL[2][oneOne]  / evidenceL[3][spots];			//needed calculation (frequency of attribute / frequency of class)
+							System.out.println(evidenceL[2][oneOne] + "/" + evidenceL[3][spots]);
+							System.out.println(probabilityPerClass[0][b] + " sdf");
+							System.out.println(spots + " spots");
+							System.out.println(oneOne + " oneOne");
+							System.out.println(b + " B");
+							
+							
+						}
+						System.out.println(b + " B2");
+						oneOne++;
 					}
 
-				}
-			}
+				//}
+			//}
 			
 		}
 		//System.out.println(evidenceL[2][100] +"spot");
@@ -272,7 +290,8 @@ public class Main {
 		System.out.println((type5Prob));
 		System.out.println((type6Prob));
 		System.out.println((type7Prob));*/
-
-				
+		//System.out.println(arr[50][NOC] + " NOC");
+		//System.out.println(probabilityPerClass[0][1] + " NOC");
+		
 	}
 }
