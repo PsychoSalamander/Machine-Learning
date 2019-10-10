27cb51eea0214ef4bfe230847ad87f67d1720e16
Blake Mitchell <mitchell.blaken@gmail.com>
2019-10-10 01:48:55 -0600

    Blake: Cleaned up comments, refactored ProcessedData.java to hold floats instead of doubles for the arrays.

---
+++ Project2/src/datapkg/DataProcessor.java
@@ -0,0 +1,31 @@
+package datapkg;
+
+public class DataProcessor {
+
+    // the file path of
+    private String FilePath;
+
+    private ProcessedData DataOutput;
+
+    DataProcessor(String FilePath) {
+	this.FilePath = FilePath;
+    }
+
+    public void loadProcessedData() {
+
+    }
+
+    public void processTheData() {
+
+    }
+
+    public boolean hasItBeenProcessed() {
+	
+	return null
+    }
+
+    public ProcessedData getProcessedData() {
+	return DataOutput;
+    }
+
+}
