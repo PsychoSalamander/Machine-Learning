f2ec410593b2b3aedbeb70bbe13f620ef566c906
Blake Mitchell <mitchell.blaken@gmail.com>
2019-10-10 01:04:28 -0600

    Blake: Refactored the ProcessedData class to be congruent with out needs for the outputs

--- Project2/src/datapkg/ProcessedData.java
+++ Project2/src/datapkg/ProcessedData.java
@@ -1,28 +1,30 @@
 package datapkg;
 
-import java.util.Dictionary;
-
 public class ProcessedData {
-	
-	private double[][] DataArray;
-	
-	private Dictionary LookupTable;
-	
-	ProcessedData(double[][] DataArray, Dictionary LookupTable)
-	{
-		this.DataArray = DataArray;
-		
-		this.LookupTable = LookupTable;
-	}
 
-	public double[][] getDataArray()
-	{
-		return DataArray;
-	}
-	
-	public Dictionary getLookupTable()
-	{
-		return LookupTable;
-	}
+    private double[][] DataArrayShuffled;    // array containing the data, after it has been processed AND shuffled
+    private double[][] DataArrayClassSorted; // array containing the data, after it has been processed AND sorted
+    private int ClassColumn; 		     // position of the class within both of the arrays
+
+    ProcessedData(double[][] DataArrayShuffled, double[][] DataArrayClassSorted, int ClassColumn) {
+	this.DataArrayShuffled = DataArrayShuffled;
+	this.DataArrayClassSorted = DataArrayClassSorted;
+	this.ClassColumn = ClassColumn;
+    }
+
+    // returns the array containing the data, after it has been processed AND
+    // shuffled
+    public double[][] getDataArrayShuffled() {
+	return DataArrayShuffled;
+    }
+
+    // returns array containing the data, after it has been processed AND sorted
+    public double[][] getDataArraySorted() {
+	return DataArrayClassSorted;
+    }
+
+    // returns array containing the data, after it has been processed AND sorted
+    public int getClassColumnPosition() {
+	return ClassColumn;
+    }
 }
-
